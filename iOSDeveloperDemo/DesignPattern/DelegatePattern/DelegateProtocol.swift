1ba2fd40f9de9646735737ad243c80816b007ce9
JieZou <jie.zou.developer@outlook.com>
2019-05-14 17:28:11 +0800

    next time must todo

---
+++ iOSDeveloperDemo/DesignPattern/DelegatePattern/DelegateProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  DelegateProtocol.swift
+//  iOSDeveloperDemo
+//
+//  Created by 邹杰 on 5/14/19.
+//  Copyright © 2019 Zoujie. All rights reserved.
+//
+
+import Foundation
+
+//实现多继承
+//NSProxy
+
