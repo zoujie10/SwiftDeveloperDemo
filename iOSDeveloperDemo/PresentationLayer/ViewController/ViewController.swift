20a960c08a42ce7ec1e0ccea5b1e610f51d7bb7f
zoujie <zj89011810@163.com>
2020-09-16 22:04:20 +0800

    swift moblie

--- iOSDeveloperDemo/PresentationLayer/ViewController/ViewController.swift
+++ iOSDeveloperDemo/PresentationLayer/ViewController/ViewController.swift
@@ -8,17 +8,30 @@
 
 import UIKit
 import RealmSwift
-
-class ViewController: UIViewController,UITextFieldDelegate,UITextViewDelegate {
-	var changeTag = Bool()
+import SnapKit
+class ViewController: UIViewController,UITextFieldDelegate,UITextViewDelegate,UITableViewDataSource,UITableViewDelegate {
 	
-	@IBOutlet var DisplayLB: UILabel!
-	@IBOutlet var NameLb: UILabel!
+	//MARK: UITableview data source
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return self.dataArray.count
+	}
 	
-	@IBOutlet var DisPlayTextView: UITextView!
-	@IBOutlet var InputNameTextField: UITextField!
-	@IBOutlet var AbstractLB: UILabel!
-	@IBOutlet var Dils: UILabel!
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell : UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+		cell.textLabel?.text = (self.dataArray[indexPath.row] as! String)
+		return cell
+	}
+	
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		print(self.dataArray[indexPath.row])
+		if(indexPath.row == 0){
+			self.navigationController?.pushViewController(SwiftBasicViewController(), animated: true)
+		}
+	}
+	
+	
+	var dataArray = NSArray()
+	var tableView = UITableView()
 	
 	@IBAction func clickSaveData(_ sender: UIButton) {
 		
@@ -35,45 +48,33 @@ class ViewController: UIViewController,UITextFieldDelegate,UITextViewDelegate {
 		try! realm.write{
 			realm.add(Swim)
 		}
-		let swimTags = realm.objects(Schedule.self)
-		for i in 0..<5 {
-			let swim = swimTags[i]
-			// ...
-			self.Dils.text = swim.GameDate as String?
-		}
+//		let swimTags = realm.objects(Schedule.self)
 
 		self.view.backgroundColor = UIColor.init(red: 0, green: 0, blue: 0, alpha: 0.5)
-	}
-	@IBAction func OKBtn(_ sender: UIButton) {
-		if changeTag == true{
-			DisplayLB.text = "Hello"
-			DisplayLB.backgroundColor = UIColor.brown
-			changeTag = false
-		}else{
-			DisplayLB.text = "GoodBye"
-			DisplayLB.backgroundColor = UIColor.blue
-			changeTag = true
-		}
-//		self.present(TextViewController(), animated: true) {
-//				print("Jump TextViewController")
-//		}
 	}
 	//MARK: 生命周期
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		changeTag = true
-		
+		self.dataArray = ["Swift 基本语法","Swift 进阶语法","视图UIView和层CALayer","ViewController","经典控件","UITableView","UICollectionView","触屏事件和触摸手势","动画声音视频","Alamofire","WKWebView与HTML5","数据持久化"]
+	
+		self.view.addSubview(self.tableView)
+		self.tableView.snp.makeConstraints { (make) in
+			make.edges.equalTo(self.view)
+		}
+		self.tableView.dataSource = self;
+		self.tableView.delegate = self;
+		self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+	}
+	
+	func notiMethod(){
 		//注册通知
-		let notificationCenter = NotificationCenter.default
-		notificationCenter.addObserver(self, selector:#selector(registerCompletion(notification:)), name:NSNotification.Name(rawValue: "RegisterCompletionNotification"), object: nil)
-		
-		
-		//监听系统通知
-		notificationCenter.addObserver(self, selector:#selector(handleEnterBackground), name: NSNotification.Name.UIApplicationDidEnterBackground, object: nil)
-		notificationCenter.addObserver(self, selector:#selector(handleEnterForeground), name: NSNotification.Name.UIApplicationWillEnterForeground, object: nil)
-		//监听内存警告⚠️
-		notificationCenter.addObserver(self, selector:#selector(handleMemoryWarning), name: NSNotification.Name.UIApplicationDidReceiveMemoryWarning, object: nil)
-
+			let notificationCenter = NotificationCenter.default
+			notificationCenter.addObserver(self, selector:#selector(registerCompletion(notification:)), name:NSNotification.Name(rawValue: "RegisterCompletionNotification"), object: nil)
+			//监听系统通知
+			notificationCenter.addObserver(self, selector:#selector(handleEnterBackground), name: NSNotification.Name.UIApplicationDidEnterBackground, object: nil)
+			notificationCenter.addObserver(self, selector:#selector(handleEnterForeground), name: NSNotification.Name.UIApplicationWillEnterForeground, object: nil)
+			//监听内存警告⚠️
+			notificationCenter.addObserver(self, selector:#selector(handleMemoryWarning), name: NSNotification.Name.UIApplicationDidReceiveMemoryWarning, object: nil)
 	}
 	override func viewWillAppear(_ animated: Bool) {
 		
@@ -81,16 +82,12 @@ class ViewController: UIViewController,UITextFieldDelegate,UITextViewDelegate {
 	override func viewWillDisappear(_ animated: Bool) {
 		
 	}
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
 	
 	@objc func registerCompletion(notification: Notification){
 		
 		let theDate : NSDictionary = notification.userInfo! as NSDictionary
 		let userID = theDate.object(forKey: "userID")
-		DisplayLB.text = userID as? String
+	
 		
 	}
 	
@@ -106,7 +103,7 @@ class ViewController: UIViewController,UITextFieldDelegate,UITextViewDelegate {
 	}
 	//MARK: 协议
 	func textFieldShouldClear(_ textField: UITextField) -> Bool {
-		InputNameTextField.text = nil
+		
 		print("clear all input Info")
 		return true
 	}
