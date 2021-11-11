b5d909e240ba274b453ae78dd7b2d839735ef85a
zoujie <jie.zou.developer@outlook.com>
2021-11-11 22:02:52 +0800

    system sound

--- iOSDeveloperDemo/Book-Moblie🐱/AnimationSoundVideo/SwiftSoundVideoVC.swift
+++ iOSDeveloperDemo/Book-Moblie🐱/AnimationSoundVideo/SwiftSoundVideoVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AudioToolbox
 
 class SwiftSoundVideoVC: UIViewController {
 
@@ -52,15 +53,37 @@ class SwiftSoundVideoVC: UIViewController {
     @objc func clickSystemSound(){
         //2.1 Sysetm Sound Services 简短声音 音效
         /**
-         
+         1.播放的声音长度要小于30s
+         2.声音文件的格式必须是PCM或IMA4
+         3.无法控制音频播放的进度
+         4.调用方法后立即播放声音，并且无法设置声音大小
+         5.无法进行循环播放或立体声播放的控制
+         6.可以调用系统的振动功能
          */
-       
+        var _soundId : SystemSoundID = 0
+        let path = Bundle.main.path(forResource: "pop_drip", ofType: "wav")
+        let soundUrl = URL(fileURLWithPath: path!)
+        AudioServicesCreateSystemSoundID(soundUrl as CFURL,&_soundId)
+       //监听音频完成
+        AudioServicesAddSystemSoundCompletion(_soundId, nil, nil, { (soundID, clientData) -> Void in
+            print("音频播放之后，重复播放。。。")
+//            AudioServicesPlaySystemSound(soundID)
+        },nil)
+        AudioServicesPlaySystemSound(_soundId)//调用音频的播放
     }
     @objc func clickSound(){
         //2.2 AVAudioPlayer 播放音乐
         /**
-         
+         1.播放任意时长的音频文件
+         2.播放文件中或者内存缓存区中的声音
+         3.进行音频文件的循环播放
+         4.使用多个AVAudioPlayer实例可以同时播放多个音频文件
+         5.控制播放的音量，设置立体声，还可以设置播放中声音的速率
+         6.支持进行声音播放的快进和后退
          */
+        
+        //Audio Queue Services可以完全实现对声音的控制,可以将声音数据从文件中读取到内存缓冲区,并对声音数据进行特殊处理,比如进行声音的快进,慢速播放,或者改变声音音色.
+        //Open AL 是一个跨平台的开源音频处理接口，为音频播放提供了一套更加底层，精细的方案，特别适合具有复杂音频使用场景的游戏开发。
     }
     @objc func clickVideo(){
         //3.1 AVPlayer播放影片
