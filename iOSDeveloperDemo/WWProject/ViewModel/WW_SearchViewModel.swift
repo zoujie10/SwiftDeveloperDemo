2a537ae2c1b34359236c1d850b2a9567c00f3a8b
zoujie <jie.zou.developer@outlook.com>
2021-12-22 22:05:33 +0800

    search page

---
+++ iOSDeveloperDemo/WWProject/ViewModel/WW_SearchViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  WW_SearchViewModel.swift
+//  iOSDeveloperDemo
+//
+//  Created by Zoujie on 2021/12/22.
+//  Copyright © 2021 Zoujie. All rights reserved.
+//
+
+import UIKit
+
+class WW_SearchViewModel: NSObject {
+
+}
