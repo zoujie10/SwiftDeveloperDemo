0b1c342367a33a8d9bea9c11adeb718a22692ab2
zoujie <jie.zou.developer@outlook.com>
2022-01-21 14:36:39 +0800

    banner cell

--- iOSDeveloperDemo/WWProject/VC/HomePage/View/WW_HomeBannerCell.swift
+++ iOSDeveloperDemo/WWProject/VC/HomePage/View/WW_HomeBannerCell.swift
@@ -7,20 +7,46 @@
 //
 
 import UIKit
+import SDCycleScrollView
+
 class WW_HomeBannerSubCell : UICollectionViewCell{
-    let urlString : String = ""
+    public var _urlString : String?
+    var urlString : String {
+        get{
+            return (_urlString)!
+        }
+        set{
+            _urlString = newValue
+            self.imageView.kf.setImage(with: URL.init(string: _urlString!))
+            self.imageView.kf.indicatorType = .activity
+        }
+    }
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.setUpSubviews()
     }
+    
     func setUpSubviews(){
+        self.contentView.addSubview(self.imageView)
+        self.contentView.addSubview(self.defaultImageView)
         
+        self.imageView.snp.makeConstraints { make in
+            make.left.equalTo(self.contentView).offset(15)
+            make.right.equalTo(self.contentView).offset(-15)
+            make.top.bottom.equalTo(self.contentView)
+        }
         
+        self.defaultImageView.snp.makeConstraints { make in
+            make.center.equalTo(self.contentView)
+        }
     }
     
     
     lazy var imageView : UIImageView = {
         let iV = UIImageView()
+        iV.backgroundColor = UIColor(r: 236, g: 236, b: 236)
+        iV.layer.cornerRadius = 8.0
+        iV.layer.masksToBounds = true
         return iV
     }()
     
@@ -35,6 +61,52 @@ class WW_HomeBannerSubCell : UICollectionViewCell{
 }
 
 
-class WW_HomeBannerCell: WW_HomeBaseCell {
+class WW_HomeBannerCell: WW_HomeBaseCell,SDCycleScrollViewDelegate {
+    typealias scrollBlock = (NSInteger) -> Void
+    var clickBlock : scrollBlock?
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setUpSubviews()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    func setUpSubviews(){
+        self.contentView.addSubview(self.cycleScrollView)
+        self.cycleScrollView.snp.makeConstraints { make in
+            make.left.right.equalTo(self.contentView)
+            make.top.equalTo(self.contentView).offset(5)
+            make.bottom.equalTo(self.contentView).offset(-5)
+        }
+        
+        self.cycleScrollView.clickItemOperationBlock = {currentIndex in
+            print("clickItemOperationBlock ---\(currentIndex)")
+        }
+        
+        self.cycleScrollView.itemDidScrollOperationBlock = { currentIndex in
+            print("itemDidScrollOperationBlock ---\(currentIndex)")
+        }
+    }
+    
+    func customCollectionViewCellClass(for view: SDCycleScrollView!) -> AnyClass! {
+        return WW_HomeBannerSubCell.classForCoder()
+    }
+    
+    func setupCustomCell(_ cell: UICollectionViewCell!, for index: Int, cycleScrollView view: SDCycleScrollView!) {
+        if index < 3{
+            let subCell = cell as! WW_HomeBannerSubCell
+            subCell.urlString = "https://hotkidceo-1251330842.cos.ap-shanghai.myqcloud.com/2021091417261400849.jpeg";
+        }
+    }
+    
+    lazy var cycleScrollView : SDCycleScrollView = {
+        let cycle = SDCycleScrollView()
+        cycle.delegate = self
+        cycle.backgroundColor = .clear
+        cycle.autoScrollTimeInterval = 3.0
+        cycle.currentPageDotImage = UIImage(named: "ww_homes_banner_page_control_default_img")
+        cycle.pageDotImage = UIImage(named: "ww_homes_banner_page_control_current_img")
+        return cycle
+    }()
 }
