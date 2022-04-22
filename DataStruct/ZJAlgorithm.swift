074662401d2280a8c5a70a29b677316552106965
zoujie10 <jie.zou.developer@outlook.com>
2022-04-22 23:33:41 +0800

    inner

--- DataStruct/ZJAlgorithm.swift
+++ DataStruct/ZJAlgorithm.swift
@@ -60,6 +60,25 @@ class ZJAlgorithm: NSObject {
     //内存优化
     func Sort_Bubbling_Inner_optimize(){
         var array = [12,14,19,14,15,12,123,1234,33]
-        
+        var swap = 0
+        var k = array.count - 1
+        var count = 0
+        for i in 0 ..< array.count {
+            var flag = true
+            count = i
+            for j in 0 ..< k{
+                if array[j] > array[j+1]{
+                    array.swapAt(j, j+1)
+                    flag = false
+                    swap = j
+                }
+            }
+            k = swap
+            if flag{
+                break
+            }
+        }
+        print(array)
+        print(count)
     }
 }
