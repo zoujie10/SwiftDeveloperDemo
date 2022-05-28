745e683da821c9b8dea50aa90930175f41b64723
zoujie <zj89011810@163.com>
2022-05-28 10:09:59 +0800

    charts change view

---
+++ WWProject🤡/MainPage/Charts/View/WW_ThirdPart_AAChartKit.swift
@@ -0,0 +1,24 @@
+//
+//  WW_ThirdPart_AAChartKit.swift
+//  iOSDeveloperDemo
+//
+//  Created by Zoujie on 2022/5/27.
+//  Copyright © 2022 Zoujie. All rights reserved.
+//
+
+import UIKit
+
+class WW_ThirdPart_AAChartKit: UIView {
+
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		configUI()
+	}
+	func configUI(){
+		
+	}
+}
