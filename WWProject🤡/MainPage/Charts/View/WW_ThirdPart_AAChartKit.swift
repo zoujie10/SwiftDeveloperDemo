38a41254540c45a680dc976a648d2da2340c15d7
zoujie <zj89011810@163.com>
2022-05-31 16:28:03 +0800

    events methods

--- WWProject🤡/MainPage/Charts/View/WW_ThirdPart_AAChartKit.swift
+++ WWProject🤡/MainPage/Charts/View/WW_ThirdPart_AAChartKit.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import AAChartKit
+import CarPlay
 
 class WW_ThirdPart_AAChartKit: UIView {
 	enum BasicChartVCChartType : Int {
@@ -21,7 +22,7 @@ class WW_ThirdPart_AAChartKit: UIView {
 		case stepArea
 		case scatter
 	}
-	var aaChartModel: AAChartModel?
+	var aaChartModel: AAChartModel = AAChartModel()
 	var chartType: BasicChartVCChartType?
 	var receivedChartType: String?
 	
@@ -43,25 +44,6 @@ class WW_ThirdPart_AAChartKit: UIView {
 	
 		self.backgroundColor = UIColor.colorWithHex(withHex: 0x4b2b7f, alpha: 1)
 		drawChart()
-	}
-
-	func drawChart(){//TODO
-		setupAAChartView()
-		let chartType = configureTheChartType()
-		setupAAChartView(with: chartType)
-		aaChartView.aa_drawChart(with: aaChartModel!)
-	}
-	func setupAAChartView(){
-		//TODO
-	}
-	func setupAAChartView(with chartType:AAChartType){
-		
-	}
-	func setupChartViewEventHandlers(){
-		self.addSubview(self.aaChartView)
-		self.aaChartView.snp.makeConstraints { make in
-			make.edges.equalTo(self)
-		}
 	}
 	func configureTheChartType() -> AAChartType {
 		switch chartType {
@@ -87,5 +69,89 @@ class WW_ThirdPart_AAChartKit: UIView {
 				break
 		}
 		return ""
+	}
+	func drawChart(){//TODO
+		setupAAChartView()
+		let chartType = configureTheChartType()
+		setupAAChartView(with: chartType)
+		aaChartView.aa_drawChart(with: aaChartModel)
+	}
+	func setupAAChartView(){
+		self.addSubview(self.aaChartView)
+		self.aaChartView.snp.makeConstraints { make in
+			make.edges.equalTo(self)
+		}
+		setupChartViewEventHandlers()
+	}
+	func setupChartViewEventHandlers(){
+		//获取图表加载完成事件
+		aaChartView.didFinishLoadHandler { chatView in
+			print("🚀🚀🚀🚀 AAChartView content did finish load!!!")
+		}
+		
+		aaChartView.clickEventHandler { aaChartView, message in
+			let messageDic = [
+				"category": message!.category,
+				"index": NSNumber(value: message!.index),
+				"name": message!.name,
+				"offset": message!.offset,
+				"x": message!.x,
+				"y": message!.y
+			] as [String : Any]
+
+			let str1 = "🖱🖱🖱🖱 clicked point series element name: \(message!.name ?? "")\n"
+			let str2 = "user finger clicked!!!,get the click event BLOCK message: \(messageDic)"
+			
+			print(str1,str2)
+		}
+		//获取图表上的手指点击及滑动事件
+		aaChartView.moveOverEventHandler { aaChartView, message in
+			let messageDic = [
+				"category": message!.category,
+				"index": NSNumber(value: message!.index),
+				"name": message!.name,
+				"offset": message!.offset,
+				"x": message!.x,
+				"y": message!.y
+			] as [String : Any]
+
+			let str1 = "👌👌👌👌 move over point series element name:\(message!.name ?? "")\n"
+			let str2 = "user finger moved over!!!,get the move over event BLOCK \(messageDic)"
+			
+			print(str1,str2)
+		}
+	}
+	func setupAAChartView(with chartType:AAChartType){
+
+		aaChartModel.chartTypeSet()("")!
+			.colorsThemeSet()(["#fe117c", "#ffc069", "#06caf4", "#7dffc0"])!
+			.tooltipValueSuffixSet()("℃")!
+			.yAxisLineWidthSet()(0)!
+			.yAxisGridLineStyleSet()(AALineStyle(width: 0))!
+			.seriesSet()([AASeriesElement()
+							.nameSet()("2017")?
+							.dataSet()([7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6])as Any,
+						  AASeriesElement()
+							.nameSet()("2018")?
+							.dataSet()([7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6])as Any,
+						  AASeriesElement()
+							.nameSet()("2019")?
+							.dataSet()([7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6])as Any,
+						  AASeriesElement()
+							.nameSet()("2020")?
+							.dataSet()([7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]) as Any])
+		
+		configureTheStyleForDifferentTypeChart()//为不同类型图表设置样式
+		configureTheYAxisPlotLineForAAChartView()/*配置 Y 轴标注线,解开注释,即可查看添加标注线之后的图表效果(NOTE:必须设置 Y 轴可见)*/
+		
+	}
+	
+	
+	
+	func configureTheStyleForDifferentTypeChart(){//为不同类型图表设置样式
+		
+	}
+	func configureTheYAxisPlotLineForAAChartView(){/*配置 Y 轴标注线,解开注释,即可查看添加标注线之后的图表效果(NOTE:必须设置 Y 轴可见)*/
+		
 	}
 }
