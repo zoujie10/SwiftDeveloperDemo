76b07433ed82811fcfb2e7e03e9daec94fa62bb6
zoujie <zj89011810@163.com>
2022-06-02 15:44:01 +0800

    main tableview

--- WWProject🤡/MainPage/Charts/WW_ChartsVC.swift
+++ WWProject🤡/MainPage/Charts/WW_ChartsVC.swift
@@ -20,17 +20,53 @@ class WW_ChartsVC: WW_MainBaseVC {
     }
     
 	func configChartsView(){
-		let chart = WW_ThirdPart_ChartsView.init()
-		view.addSubview(chart)
-		chart.snp.makeConstraints { make in
-			make.top.equalTo(self.view).offset(15)
-			make.left.right.equalTo(self.view)
-			make.height.equalTo(self.view.bounds.height/2-64)
+		view.addSubview(mainTableView)
+		mainTableView.snp.makeConstraints { make in
+			make.edges.equalTo(view)
 		}
+//		let aaChart = WW_ThirdPart_AAChartKit.init()
+//		aaChart.frame = CGRect(x: 0, y: self.view.bounds.height/2, width: WWScreenWidth, height: WWScreenHeight-self.view.bounds.height/2-64)
+//		view.addSubview(aaChart)
 		
-		let aaChart = WW_ThirdPart_AAChartKit.init()
-		aaChart.frame = CGRect(x: 0, y: self.view.bounds.height/2, width: WWScreenWidth, height: WWScreenHeight-self.view.bounds.height/2-64)
-		view.addSubview(aaChart)
-		
+	}
+	lazy var mainTableView : UITableView = {
+		let tb = UITableView.init(frame: .zero, style: .plain)
+		tb.register(UITableViewCell.classForCoder(), forCellReuseIdentifier: "cell")
+		tb.backgroundColor = .white
+		tb.delegate = self
+		tb.dataSource = self
+		return tb
+	}()
+}
+extension WW_ChartsVC:UITableViewDelegate,UITableViewDataSource{
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 3
+	}
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+		if indexPath.row == 0{
+			let chart = WW_ThirdPart_ChartsView.init()
+			cell.contentView.addSubview(chart)
+			chart.snp.makeConstraints { make in
+				make.top.equalTo(cell.contentView).offset(10)
+				make.left.right.equalTo(cell.contentView)
+				make.height.equalTo(self.view.bounds.height/2)
+			}
+		}else if indexPath.row == 1{
+			let aaChart = WW_ThirdPart_AAChartKit.init()
+			cell.contentView.addSubview(aaChart)
+			aaChart.frame = CGRect(x: 0, y: 0, width: WWScreenWidth, height: WWScreenHeight-self.view.bounds.height/2)
+		}
+		return cell
+	}
+	
+	func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+		if indexPath.row == 0{
+			return self.view.bounds.height/2+10
+		}else if indexPath.row == 1{
+			return WWScreenHeight-self.view.bounds.height/2+10
+		}else{
+			return 150
+		}
 	}
 }
