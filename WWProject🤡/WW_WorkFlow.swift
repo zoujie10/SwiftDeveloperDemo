1bb352e5565aaa6b96b608290c34fc9dd98aec21
zoujie <zj89011810@163.com>
2022-06-09 22:29:10 +0800

    ww work flow

---
+++ WWProject🤡/WW_WorkFlow.swift
@@ -0,0 +1,27 @@
+//
+//  WW_WorkFlow.swift
+//  iOSDeveloperDemo
+//
+//  Created by Zoujie on 2022/6/9.
+//  Copyright © 2022 Zoujie. All rights reserved.
+//
+
+import UIKit
+
+class WW_WorkFlow: NSObject {
+
+}
+/*
+## 流程模板： 工作时间控制在20%
+
+1.WPApiUrlConfig  配置url
+
+2.WWOdooClient 配置 接口
+
+3.自定义 ViewModel 编写 接口/逻辑方法
+3.1 单列表
+3.2 切换列表
+
+4.VC UI
+4.View/Cell 代码块
+*/
