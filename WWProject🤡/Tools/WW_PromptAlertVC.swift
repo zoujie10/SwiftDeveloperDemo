55584dedd53627d25bd16fa19a4a2857096d0077
zoujie <zj89011810@163.com>
2022-08-04 20:36:04 +0800

    tips

---
+++ WWProject🤡/Tools/WW_PromptAlertVC.swift
@@ -0,0 +1,30 @@
+//
+//  WW_PromptAlertVC.swift
+//  iOSDeveloperDemo
+//
+//  Created by Zoujie on 2022/8/3.
+//  Copyright © 2022 Zoujie. All rights reserved.
+//
+
+import UIKit
+
+class WW_PromptAlertVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
