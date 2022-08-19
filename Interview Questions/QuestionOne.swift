56a60f5267383244edbb4ff9a7cb59c2e2d69fbe
zoujie <zj89011810@163.com>
2022-08-19 20:40:34 +0800

    interview questions

---
+++ Interview Questions/QuestionOne.swift
@@ -0,0 +1,18 @@
+//
+//  QuestionOne.swift
+//  iOSDeveloperDemo
+//
+//  Created by Zoujie on 2022/8/19.
+//  Copyright © 2022 Zoujie. All rights reserved.
+//
+
+import UIKit
+//被问到的一些面试问题
+//1.conversion 与 自定义 init 的区别
+//2.https与http  http需要三次握手吗
+//3.unown关键词 和weak的区别
+//4.iOS 系统推送
+//5.基础数据类型是否可以指向 指针
+class QuestionOne: NSObject {
+
+}
