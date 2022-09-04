398aa5b37b9bfb82083029665a5837dab4d0ac8e
zoujie <zj89011810@163.com>
2022-09-04 22:37:36 +0800

    control flow

--- PerformanceOptimization/GoodCode_Practice.swift
+++ PerformanceOptimization/GoodCode_Practice.swift
@@ -42,3 +42,27 @@ class GoodCode_Practice: NSObject {
  数据不使用 - 数据并不读取只是用来传递。
  解耦 - 1.组件间通信解耦使用HashMap传递参数进行通信。2.跨技术栈边界的场景，混合栈间通信/前后端通信使用HashMap/JSON进行通信。
  */
+/*2
+ 控制流
+
+ 使用filter/reduce/map代替for循环
+
+ 使用filter/reduce/map可以带来很多好处，包括更少的局部变量，减少模板代码，代码更加清晰，可读性更高。
+
+ 不推荐
+
+ let nums = [1, 2, 3]
+ var result = []
+ for num in nums {
+	 if num < 3 {
+		 result.append(String(num))
+	 }
+ }
+ // result = ["1", "2"]
+ 复制代码
+ 推荐
+
+ let nums = [1, 2, 3]
+ let result = nums.filter { $0 < 3 }.map { String($0) }
+ // result = ["1", "2"]
+ **/
